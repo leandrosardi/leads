635da40eafbf7cc255a64d7d7ee76e453355d66c
DEV1\Leandro <leandro.sardi@expandedventure.com>
2022-06-19 18:01:18 -0300

    #7

--- app.rb
+++ app.rb
@@ -1,60 +1,60 @@
 # public screens (landing page)
 get "/leads/landing", :agent => /(.*)/ do
-    erb :"/extensions/leads/views/exports", :layout => :"/leads/layouts/public"
+    erb :"/extensions/leads/views/landing", :layout => :"/views/layouts/public"
 end
 
 # internal wizard (funnel) screens
 get "/leads/step1", :auth1 => true, :agent => /(.*)/ do
-    erb :"/extensions/leads/views/step1", :layout => :"/leads/layouts/public"
+    erb :"/extensions/leads/views/step1", :layout => :"/views/layouts/public"
 end
 
 get "/leads/step2", :auth1 => true, :agent => /(.*)/ do
-    erb :"/extensions/leads/views/step2", :layout => :"/leads/layouts/public"
+    erb :"/extensions/leads/views/step2", :layout => :"/views/layouts/public"
 end
 
 get "/leads/tripwire", :auth1 => true, :agent => /(.*)/ do
-    erb :"/extensions/leads/views/tripwire", :layout => :"/leads/layouts/public"
+    erb :"/extensions/leads/views/tripwire", :layout => :"/views/layouts/public"
 end
 
 # internal app screens
 get "/leads/plans", :auth1 => true, :agent => /(.*)/ do
-    erb :"/extensions/leads/views/plans", :layout => :"/layouts/core"
+    erb :"/extensions/leads/views/plans", :layout => ":/views/layouts/core"
 end
 
 get "/leads/welcome", :auth1 => true, :agent => /(.*)/ do
-    erb :"/extensions/leads/views/welcome", :layout => :"/layouts/core"
+    erb :"/extensions/leads/views/welcome", :layout => ":/views/layouts/core"
 end
 
 get "/leads/search", :auth1 => true, :agent => /(.*)/ do
-    erb :"/extensions/leads/views/search", :layout => :"/layouts/core"
+    erb :"/extensions/leads/views/search", :layout => ":/views/layouts/core"
 end
 
 get "/leads/saved_searches", :auth1 => true, :agent => /(.*)/ do
-    erb :"/extensions/leads/views/saved_searches", :layout => :"/layouts/core"
+    erb :"/extensions/leads/views/saved_searches", :layout => ":/views/layouts/core"
 end
 
 get "/leads/results", :auth1 => true, :agent => /(.*)/ do
-    erb :"/extensions/leads/views/results", :layout => :"/layouts/core"
+    erb :"/extensions/leads/views/results", :layout => ":/views/layouts/core"
 end
 
 get "/leads/exports", :auth1 => true, :agent => /(.*)/ do
-    erb :"/extensions/leads/views/exports", :layout => :"/layouts/core"
+    erb :"/extensions/leads/views/exports", :layout => ":/views/layouts/core"
 end
 
 # filters
-get "/leads/filter_landing"
+get "/leads/filter_landing" do
     erb :"/extensions/leads/views/filter_landing"
 end
 
-get "/leads/filter_step1", :auth1 => true
+get "/leads/filter_step1", :auth1 => true do
     erb :"/extensions/leads/views/filter_step1"
 end
 
-get "/leads/filter_step2", :auth1 => true
+get "/leads/filter_step2", :auth1 => true do
     erb :"/extensions/leads/views/filter_step2"
 end
 
-get "/leads/filter_tripwire", :auth1 => true
+get "/leads/filter_tripwire", :auth1 => true do
     erb :"/extensions/leads/views/filter_tripwire"
 end
 
