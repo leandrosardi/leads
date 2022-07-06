dbee0bcbcdb1ca4d0447f9a1620b46b12981ddfe
leandrosardi <leandro.sardi@expandedventure.com>
2022-07-06 13:41:29 -0300

    #13

--- models/skeleton/fl_search.rb
+++ models/skeleton/fl_search.rb
@@ -1,5 +1,7 @@
 module Leads
   class FlSearch < Sequel::Model(:fl_search)
+    include BlackStack::TableHelper
+
     many_to_one :user, :class=>:'BlackStack::MySaaS::User', :key=>:id_user
     one_to_many :positions, :class=>'Leads::FlSearchPosition', :key=>:id_search
     one_to_many :locations, :class=>'Leads::FlSearchLocation', :key=>:id_search
@@ -138,52 +140,7 @@ module Leads
       self
     end
 
-
-
-
-
-    # TODO: move this to a super-module called BlackStack::MySaas::Paginable 
-    def validate_pagination_descriptor(h)
-      errors = []
-
-      # validation: h must be a hash
-      errors << "h must be a hash" if !h.is_a?(Hash)
-
-      if h.is_a?(Hash)
-        # validation: :page is required
-        errors << "page is required" if h['page'].nil?
-
-        # validation: :pagesize is required
-        errors << "pagesize is required" if h['pagesize'].nil?
-
-        # validation: :sortcolumn is required
-        errors << "sortcolumn is required" if h['sortcolumn'].nil?
-
-        # validation: :sortorder is required
-        errors << "sortorder is required" if h['sortorder'].nil?
-
-        # validation: the value of :sortcolumn must be a valid string
-        errors << "sortcolumn must be a valid string" if !h['sortcolumn'].nil? && !h['sortcolumn'].is_a?(String)
-
-        # validation: the value of :sortorder must be a valid string or nil
-        errors << "sortorder must be a valid string" if !h['sortorder'].nil? && !h['sortorder'].is_a?(String)
-
-        # validation: if :sortorder is a string, it must be either 'asc' or 'desc'
-        if !h['sortorder'].nil? && h['sortorder'].is_a?(String)
-          errors << "sortorder must be either 'asc' or 'desc'" if h['sortorder']!='asc' && h['sortorder']!='desc'
-        end
-
-        # validation: the value of :page must be a valid integer or nil
-        errors << "page must be a valid integer" if !h['page'].nil? && !h['page'].is_a?(Integer)
-
-        # validation: the value of :pagesize must be a valid integer or nil
-        errors << "pagesize must be a valid integer" if !h['pagesize'].nil? && !h['pagesize'].is_a?(Integer)
-      end
-
-      errors
-    end
-
-    # TODO: move this to a super-module called BlackStack::MySaas::Paginable, as an abstract method.
+    # This method works with the TableHelper module.
     # Return the FROM-WHERE part of the SQL query to retrieve the results of the table, with not pagination nor sorting, nor listed columns.
     # This method is used to build custom queries on other methods.
     def core
@@ -253,68 +210,7 @@ module Leads
       q0
     end
 
-    # total number of unique leads matching with this search
-    # TODO: move this to a super-module called BlackStack::MySaas::Paginable, as an abstract method.
-    def count
-      DB["SELECT COUNT(id) AS n #{self.core}"].first[:n]
-    end
-
-    # TODO: move this to a super-module called BlackStack::MySaas::Paginable 
-    # return a hash descriptor with the sattus of the pagination: row_from, row_to, total_rows, total_pages, page (after revision)
-    def status(h)
-      # validate the pagination descriptor
-      errors = self.validate_pagination_descriptor(h)
-
-      # raise an exception if there are errors
-      raise "Errors found:\n#{errors.join("\n")}" if errors.size>0
-
-      # get parameters
-      page_number = h['page'].to_i
-      page_size = h['pagesize'].to_i
-
-      total_rows = self.count
-      total_pages = (total_rows.to_f/page_size.to_f).ceil
-
-      # pagination correction to prevent glitches
-      page_number = 1 if page_number < 1
-      page_number = total_pages if page_number > total_pages
-
-      # calculate info for showing at the bottom of the table
-      from_row = (page_number.to_i-1) * page_size.to_i + 1
-      to_row = [page_number*page_size, total_rows].min
-
-      # return
-      {
-        'row_from' => from_row,
-        'row_to' => to_row,
-        'total_rows' => total_rows,
-        'total_pages' => total_pages,
-        'page' => page_number
-      }
-    end
-
-
-    def self.validate_columns_descriptor(h)
-      errors = []
-
-      # validate: h must be a hash
-      errors << "columns must be a hash" if !h.is_a?(Hash)
-
-      # other validations
-      if h.is_a?(Hash)
-        # validattion: :query_field is required
-        errors << "query_field is required" if h['query_field'].nil?
-
-        # validation: :query_field must be a string
-        errors << "query_field must be a string" if !h['query_field'].nil? && !h['query_field'].is_a?(String)
-      end
-
-      # return
-      errors
-    end
-
-
-    # TODO: move this to a super-module called BlackStack::MySaas::Paginable as an abstract method
+    # This method works with the TableHelper module.
     # return an array of hashes with the columns of the table
     def columns
       [
@@ -343,33 +239,7 @@ module Leads
       ]
     end
 
-    # TODO: move this to a super-module called BlackStack::MySaas::Paginable
-    # return an array of leads, filtering by the parameters of this search, and paginating and sorting by the parameters received.
-    def rows(h)
-      # validate the pagination descriptor
-      errors = self.validate_pagination_descriptor(h)
-
-      # raise an exception if there are errors
-      raise "Errors found:\n#{errors.join("\n")}" if errors.size>0
-
-      # get pagination status
-      p = self.status(h)
-
-      # get the list of leads
-      column_names = self.columns.map {|c| c['query_field'] }
-      q = "
-        SELECT #{column_names.join(', ')}
-        #{self.core}
-        -- sorting
-        ORDER BY l.#{h['sortcolumn']} #{h['sortorder']}
-        -- pagination
-        LIMIT #{h['pagesize']}
-        OFFSET #{p['row_from']}
-      "
-      DB[q].all
-    end 
-
-    # reurn
+    # reurn an array of lead objects who match with the filters of the search.
     def leads(h)
       self.rows(h).map { |row| 
         Leads::FlLead.where(:id=>row[:id]).first 
@@ -383,7 +253,5 @@ module Leads
     def count_companies
       DB["SELECT COUNT(DISTINCT id_company) AS n #{self.core}"].first[:n]
     end
-
-
-  end
-end
+  end # class FlSearch
+end # module Leads
