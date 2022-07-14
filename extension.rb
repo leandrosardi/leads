a0782f93486ec39be9cd58a110c7436b93f0f887
leandrosardi <leandro.sardi@expandedventure.com>
2022-07-14 11:05:07 -0300

    #54

--- extension.rb
+++ extension.rb
@@ -33,4 +33,22 @@ BlackStack::Extensions::add ({
     :storage_folders => [
         { :name => 'leads.exports', },
     ],
+
+    # deployment routines
+    :deployment_routines => [{
+        :name => 'start-mysaas',
+        :commands => [{ 
+            # back up old configuration file
+            # setup new configuration file
+            :command => "
+                source /home/%ssh_username%/.rvm/scripts/rvm; rvm install 3.1.2; rvm --default use 3.1.2 > /dev/null 2>&1;
+                cd /home/%ssh_username%/code/mysaas/extensions/leads/p > /dev/null 2>&1; 
+                export RUBYLIB=/home/%ssh_username%/code/mysaas > /dev/null 2>&1;
+                nohup ruby export.rb > /dev/null 2>&1 &
+            ",
+            :matches => [ /Already installed ruby/ ],
+            #:nomatches => [ { :nomatch => /.+/, :error_description => 'No output expected.' } ],
+            :sudo => false,
+        }],
+    }],
 })
